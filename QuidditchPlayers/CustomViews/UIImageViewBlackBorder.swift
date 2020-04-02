f59f011b19aa454bba284b30bf17e70e13746d92
Chris Jones <chrisjmobileapps@gmail.com>
2020-04-02 12:15:37 -0500

    Add black border to image view

---
+++ QuidditchPlayers/CustomViews/UIImageViewBlackBorder.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageViewBlackBorder.swift
+//  QuidditchPlayers
+//
+//  Created by Chris Jones on 4/2/20.
+//  Copyright © 2020 cjmobileapps. All rights reserved.
+//
+
+import UIKit
+
+class UIImageViewBlackBorder: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUIImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUIImageView()
+    }
+
+    func setupUIImageView() {
+        layer.cornerRadius = 8.0
+        layer.masksToBounds = true
+        layer.borderColor = Colors.black.cgColor
+        layer.borderWidth = 2
+    }
+}
